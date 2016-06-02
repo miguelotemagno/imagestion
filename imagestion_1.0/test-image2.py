884a6271b127c8f841c6efac3986d1e1cb9d434c
Miguel Vargas <mvargas@ionix.cl>
2016-06-02 14:53:24 -0400

    obtencion de bordes en imagen

--- imagestion_1.0/test-image2.py
+++ imagestion_1.0/test-image2.py
@@ -6,36 +6,42 @@ import numpy as np
 import Image
 import sys
 
+# Referencias: http://www.scipy-lectures.org/advanced/image_processing/
+#  http://gis.stackexchange.com/questions/24827/how-to-smooth-the-polygons-in-a-contour-map
+
 imgFile = sys.argv[1]
 print imgFile
 
 
-im = Image.open(imgFile)
-im.show()
-#r,g,b = im.split()
+#im = Image.open(imgFile)
+#im.show()
 
-#r = ndimage.binary_dilation(r)
-#g = ndimage.binary_dilation(g)
-#b = ndimage.binary_dilation(b)
-
-#r = ndimage.binary_erosion(r)
-#g = ndimage.binary_erosion(g)
-#b = ndimage.binary_erosion(b)
-
-#rgb = Image.merge("RGB", (r,g,b))
-#rgb.show()
-shape1 = (5,5)
-shape2 = (5,5)
+shape1 = (3,3)
+shape2 = (6,6)
 
 im1 = ndimage.imread(imgFile, flatten=True).astype(np.uint8)
 print im1.shape
 im2 = ndimage.grey_erosion(im1, size=(shape1)) 
 im2 = ndimage.grey_dilation(im2, size=(shape1)) 
-#toimage(im2).show()
 
 im3 = ndimage.grey_dilation(im1, size=(shape2))
-im3 = ndimage.grey_erosion(im3, size=(shape2)) 
+#im3 = ndimage.grey_erosion(im3, size=(shape2)) 
 
-diff = im2 - im3
-
+diff = im3 - im2
 toimage(diff).show()
+
+#im4 = ndimage.grey_erosion(diff, size=(shape1)) 
+#im4 = ndimage.grey_dilation(im4, size=(shape1)) 
+
+#im5 = ndimage.grey_dilation(diff, size=(shape2))
+#im5 = ndimage.grey_erosion(im5, size=(shape2)) 
+
+#diff2 = im5 - im4
+
+#im4 = ndimage.rotate(diff, 15, mode='constant')
+im4 = ndimage.gaussian_filter(diff, 2)
+sx = ndimage.sobel(im4, axis=0, mode='constant')
+sy = ndimage.sobel(im4, axis=1, mode='constant')
+sob = np.hypot(sx, sy)
+
+toimage(sob).show()
