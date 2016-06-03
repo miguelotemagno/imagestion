6d8369ffea27bf85b4d2e9ef8560831f1a81c85e
Miguel Vargas <mvargas@ionix.cl>
2016-06-02 20:46:27 -0400

    trabajo sobre filtros

--- imagestion_1.0/test-image2.py
+++ imagestion_1.0/test-image2.py
@@ -39,9 +39,26 @@ toimage(diff).show()
 #diff2 = im5 - im4
 
 #im4 = ndimage.rotate(diff, 15, mode='constant')
-im4 = ndimage.gaussian_filter(diff, 2)
-sx = ndimage.sobel(im4, axis=0, mode='constant')
-sy = ndimage.sobel(im4, axis=1, mode='constant')
-sob = np.hypot(sx, sy)
+#im4 = ndimage.gaussian_filter(diff, 2)
+#sx = ndimage.sobel(im4, axis=0, mode='constant')
+#sy = ndimage.sobel(im4, axis=1, mode='constant')
+#sob = np.hypot(sx, sy)
+#
+#toimage(sob).show()
 
-toimage(sob).show()
+
+print diff
+print np.amin(diff)
+print np.amax(diff)
+
+filter = (np.amin(diff) + np.amax(diff)) // 8
+print filter
+
+im5 = np.array(diff, np.uint8)
+im5[im5 < filter] = 0
+im5 = ndimage.grey_erosion(im5, size=(shape1)) 
+
+im = Image.fromarray(im5)
+im6 = Image.merge('RGB',(im,im,im))
+
+toimage(im6).show()
