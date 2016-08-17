17b1b46e6f8354f8469a3fd8ca7e0e18f6474f50
Miguel Vargas <mvargas@ionix.cl>
2016-08-17 15:02:38 -0300

    se construye segmentador via ANN

--- imagestion_1.0/test-segmentation-ANN.py
+++ imagestion_1.0/test-segmentation-ANN.py
@@ -21,40 +21,6 @@ from ANN import *
 
 #-----------------------------------------------------------------------
 
-def HSVColor(img):
-    if isinstance(img,Image.Image):
-        r,g,b = img.split()
-        Hdat = []
-        Sdat = []
-        Vdat = [] 
-        for rd,gn,bl in zip(r.getdata(),g.getdata(),b.getdata()) :
-            h,s,v = colorsys.rgb_to_hsv(rd/255.,gn/255.,bl/255.)
-            Hdat.append(int(h*255.))
-            Sdat.append(int(s*255.))
-            Vdat.append(int(v*255.))
-        r.putdata(Hdat)
-        g.putdata(Sdat)
-        b.putdata(Vdat)
-        return Image.merge('RGB',(r,g,b))
-    else:
-        return None
-        
-def plotHistogram(arr, b):
-    hist, bins = np.histogram(arr, bins=b)
-    #width = 0.7 * (bins[1] - bins[0])
-    center = (bins[:-1] + bins[1:]) / 2
-    plt.bar(center, hist, align='center') #, width=width)
-    plt.show()
-    
-# http://stackoverflow.com/questions/16373425/add-text-on-image-using-pil 
-def showImage(img, text):
-    draw = ImageDraw.Draw(img)
-    # font = ImageFont.truetype(<font-file>, <font-size>)
-    font = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 16)
-    # draw.text((x, y),"Sample Text",(r,g,b))
-    draw.text((10, 10),text) #,font=font,fill=(255,255,255))
-    img.show()
-
 def evalPixel(pix, net):
 	r,g,b = pix
 	pixel = np.array([float(r)/255, float(g)/255, float(b)/255])
@@ -117,42 +83,8 @@ diff2 = img2 - img1
 ds = [] #SupervisedDataSet(3, 1)
 hist = {}
 muestra1 = []
-muestra2 = []
+muestra2 = []		
 
-i = 0
-for y in range(seg.height):
-	if i > 10:
-		break
-	for x in range(seg.width):
-		r,g,b = piel.getpixel((x,y))
-		key = "x%02x%02x%02x" % (r, g, b)
-		hist[key] = hist[key] + 1 if key in hist else 0
-		
-		if (r|g|b and hist[key] == 0) :
-			muestra1 = [float(r)/256, float(g)/256, float(b)/256]
-			print "%05d (%02x, %02x, %02x) => [1] %s" % (i,r,g,b, muestra1)
-			## ds.addSample((muestra1[0], muestra1[1], muestra1[2]), (1))
-			ds.append([muestra1, [1]])
-			i += 1
-			
-i = 0
-for y in range(seg.height):
-	if i > 10:
-		break
-	for x in range(seg.width):
-		r,g,b = fondo.getpixel((x,y))
-		key = "x%02x%02x%02x" % (r, g, b)
-		hist[key] = hist[key] + 1 if key in hist else 0
-		
-		if (r|g|b and hist[key] == 0) :
-			muestra2 = [float(r)/255, float(g)/255, float(b)/255]
-			print "%05d (%02x, %02x, %02x) => [0] %s" % (i,r,g,b, muestra2)
-			## ds.addSample((muestra2[0], muestra2[1], muestra2[2]), (0))
-			ds.append([muestra2, [0]])
-			i += 1
-			
-
-print '4.- Training network ############################################'
 #http://pybrain.org/docs/api/supervised/trainers.html#pybrain.supervised.trainers.BackpropTrainer
 #http://pybrain.org/docs/quickstart/training.html
 ## net = buildNetwork(3, 3, 1, bias=True, hiddenclass=TanhLayer)
@@ -180,39 +112,17 @@ net = ANN(3, 4, 1, threshold)
 if os.path.isfile(dbFile) :
 	print '3.- Restore previous session #########################'
 	net.load(dbFile)
-else:
-	net.iniciar_perceptron()
 
-net.entrenar_perceptron(ds,epochs)
+	print '5.- Perform segmentation #########################'
+	toimage(rgb).show()
 
-print '5.- Perform segmentation #########################'
-toimage(piel).show()
+	## for yy in range(seg.height):
+		## for xx in range(seg.width):
+			## if (evalPixel(rgb.getpixel((xx,yy)), net) < 0.6) :
+				## rgb.putpixel((xx,yy), 0)
 
-## for yy in range(seg.height):
-	## for xx in range(seg.width):
-		## if (evalPixel(rgb.getpixel((xx,yy)), net) < 0.6) :
-			## rgb.putpixel((xx,yy), 0)
+	im = np.array(rgb)
+	#im[evalPixel(x, net) < 0.6] = 0
+	rgb = [[x if evalPixel(x, net) > 0.6 else (0,0,0) for x in row] for row in im]
 
-im = np.array(rgb)
-#im[evalPixel(x, net) < 0.6] = 0
-rgb = [[x if evalPixel(x, net) > 0.6 else (0,0,0) for x in row] for row in im]
-
-toimage(rgb).show()
-
-#http://stackoverflow.com/questions/6006187/how-to-save-and-recover-pybrain-training
-## fileObject = open('neuralnet-ann.bkp', 'w')
-
-print "write changes? (y/n): "
-k = sys.stdin.read(1)
-ch = sys.stdin.readline()
-
-if k == 'y':
-	print '6.- Store data to file #########################'
-	net.save(dbFile)
-
-else:
-	print 'Changes dismissed'
-	
-
-
-
+	toimage(rgb).show()
