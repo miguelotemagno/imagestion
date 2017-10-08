54a8248c2e5c25f7b6d06464b5ca8b30a3814d4e
miguelotemagno <miguelwelch@gmail.com>
2017-10-07 21:07:34 -0300

    Se inicia nueva aplicacion de redes neuronales. Se intentara la clasificacion de palabras

---
+++ textClassification/classify.py
@@ -0,0 +1,69 @@
+# +-----------------------------------------------------------------------+
+# | IMAGESTION                                                            |
+# |                                                                       |
+# | Copyright (C) 2010-Today, GNUCHILE.CL  -  Santiago de Chile           |
+# | Licensed under the GNU GPL                                            |
+# |                                                                       |
+# | Redistribution and use in source and binary forms, with or without    |
+# | modification, are permitted provided that the following conditions    |
+# | are met:                                                              |
+# |                                                                       |
+# | o Redistributions of source code must retain the above copyright      |
+# |   notice, this list of conditions and the following disclaimer.       |
+# | o Redistributions in binary form must reproduce the above copyright   |
+# |   notice, this list of conditions and the following disclaimer in the |
+# |   documentation and/or other materials provided with the distribution.|
+# | o The names of the authors may not be used to endorse or promote      |
+# |   products derived from this software without specific prior written  |
+# |   permission.                                                         |
+# |                                                                       |
+# | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   |
+# | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     |
+# | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR |
+# | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  |
+# | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, |
+# | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT      |
+# | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, |
+# | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY |
+# | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT   |
+# | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE |
+# | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  |
+# |                                                                       |
+# +-----------------------------------------------------------------------+
+# | Author: Miguel Vargas Welch <miguelote@gmail.com>                     |
+# +-----------------------------------------------------------------------+
+
+import zlib
+import numpy as np
+from ANN import *
+
+class Classify(object):
+	def __init__(self):
+		self.net = ANN(3, 4, 1)
+		self.filter = None
+		self.fromFile = 'cat'
+		self.fromWeb = 'links -dump'
+		self.command = "%s %s | tr -sc 'A-Za-z' '\n' | tr 'A-Z' 'a-z' | sort | uniq -c"
+		self.text = ""
+		pass
+
+	def getCRC(self, text):
+		maxValue = 0xffffffff * 1.0
+		crc = zlib.crc32(text) % (1<<32)
+		return crc/maxValue
+
+	def loadFilter(self, file):
+		self.filter = ANN(3, 4, 1)
+		self.filter.load(file)
+
+	def saveNet(self,file):
+		self.net.save(file)
+
+	def loadFromFile(self,source):
+		self.text = self.command % (self.fromFile, source)
+
+	def loadFromWeb(self,source):
+		self.text = self.command % (self.fromWeb, source)
+
+
+
