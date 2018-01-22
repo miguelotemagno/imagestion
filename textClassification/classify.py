74620bdac3adb30a844e90f03ef925a310dda367
miguelotemagno <miguelwelch@gmail.com>
2018-01-22 12:14:09 -0300

    remodelamiento de red neuronal

--- textClassification/classify.py
+++ textClassification/classify.py
@@ -62,8 +62,7 @@ class Classify:
 	def getBase32(self, text):
 		result = i = 0
 		
-		#for letter in list(reversed(list(text.upper()))):
-		for letter in list(text.upper()):		
+		for letter in list(reversed(list(text.upper()))):
 			result += (ord(letter) - 65) * (32**i)
 			i += 1	
 			
@@ -72,10 +71,8 @@ class Classify:
 	##########################################################################		           
 	
 	def getCRC(self, text):
-		#crc = zlib.crc32(text) % (1<<32)
 		crc = self.getBase32(text)
 		return crc/self.maxValue		
-		#return log(crc) #crc/maxValue
 
 	##########################################################################		           
 	
@@ -97,16 +94,23 @@ class Classify:
 		
 		tf.reset_default_graph()
 		
-		HIDDEN_NODES = 3
+		INPUTS = 3
+		HIDDEN_NODES = 4
 		self.x = tf.placeholder("float", [None, 3], name="x")
 		self.y_ = tf.placeholder("float", [None, 1], name="y_")		
 		
-		W = tf.get_variable("W", shape=[3, HIDDEN_NODES]) #, initializer = tf.zeros_initializer)
-		b = tf.get_variable("b", shape=[HIDDEN_NODES]) #, initializer = tf.zeros_initializer)
-		W2 = tf.get_variable("W2", shape=[HIDDEN_NODES,2]) #, initializer = tf.zeros_initializer)
-		b2 = tf.get_variable("b2", shape=[2]) #, initializer = tf.zeros_initializer)
+		W = tf.get_variable("W", shape=[INPUTS, HIDDEN_NODES])
+		b = tf.get_variable("b", shape=[HIDDEN_NODES])
+		W2 = tf.get_variable("W2", shape=[HIDDEN_NODES,3]) 
+		b2 = tf.get_variable("b2", shape=[3]) 
+		W3 = tf.get_variable("W3", shape=[3,2]) 
+		b3 = tf.get_variable("b3", shape=[2]) 
 		
-		self.y = tf.nn.softmax( tf.matmul( tf.nn.relu( tf.matmul(self.x,W) + b), W2))		
+		#self.y = tf.nn.softmax( tf.matmul( tf.nn.relu( tf.matmul(self.x,W) + b), W2))		
+		layer1 = tf.matmul(self.x,W) + b
+		layer2 = tf.matmul(tf.nn.relu(layer1), W2) + b2
+		layer3 = tf.matmul(tf.nn.relu(layer2), W3) + b3
+		self.y = tf.nn.softmax(layer3)		
 				
 		self.filter = tf.Session()
 		saver = tf.train.Saver()		
@@ -116,14 +120,22 @@ class Classify:
 		
 	def defineFilterModel(self):
 		print "=> defineFilterModel\n"
-		HIDDEN_NODES = 3
-		self.x = tf.placeholder("float", [None, 3], name="x")
+		INPUTS = 3
+		HIDDEN_NODES = 4
+		self.x = tf.placeholder("float", [None, INPUTS], name="x")
 		self.y_ = tf.placeholder("float", [None, 1], name="y_")
-		W = tf.Variable(tf.random_uniform([3, HIDDEN_NODES], -.01, .01), name="W")
+		
+		W = tf.Variable(tf.random_uniform([INPUTS, HIDDEN_NODES], -.01, .01), name="W")
 		b = tf.Variable(tf.random_uniform([HIDDEN_NODES], -.01, .01), name="b")
-		W2 = tf.Variable(tf.random_uniform([HIDDEN_NODES, 2], -.1, .1), name="W2")
-		b2 = tf.Variable(tf.zeros([2]), name="b2")
-		self.y = tf.nn.softmax( tf.matmul( tf.nn.relu( tf.matmul(self.x,W) + b), W2))
+		W2 = tf.Variable(tf.random_uniform([HIDDEN_NODES, 3], -.01, .01), name="W2")
+		b2 = tf.Variable(tf.zeros([3]), name="b2")
+		W3 = tf.Variable(tf.random_uniform([3, 2], -.1, .1), name="W3")
+		b3 = tf.Variable(tf.zeros([2]), name="b3")
+		
+		layer1 = tf.matmul(self.x,W) + b
+		layer2 = tf.matmul(tf.nn.relu(layer1), W2) + b2
+		layer3 = tf.matmul(tf.nn.relu(layer2), W3) + b3
+		self.y = tf.nn.softmax(layer3)
 
 		self.cross_entropy = -tf.reduce_sum(self.y_ * tf.log(self.y))
 		return tf.train.GradientDescentOptimizer(0.2).minimize(self.cross_entropy)		
@@ -151,14 +163,14 @@ class Classify:
 
 		for i in xrange(len(words)):
 			(word, crc, n, lenw) = words[i]
-			print "%s: [%f20] [%s] [%f]" % (word, crc, n, lenw)
+			print "%s: [%1.15f] [%s] [%f]" % (word, crc, n, lenw)
 			#data = [val, crc, len(word)/maxLen]
 			data = [0.5, crc, lenw]
 			#trainData.append([data, [0]])
 			trainData.append(data)
 			expect.append([0.0])
 
-			if i%5 == 0:
+			if i%3 == 0:
 				wrd = self.wordGenerate()
 				gen = self.getCRC(wrd) 
 				data = [0.0005, gen, len(wrd)]
@@ -264,11 +276,11 @@ class Classify:
 		init = tf.global_variables_initializer()
 
 		self.sess.run(init)
-		for step in range(10000):
+		for step in range(1000):
 			feed_dict = {self.x: xTrain, self.y_: yTrain}  # feed the net with our inputs and desired outputs.
 			e, a = self.sess.run([self.cross_entropy, train_step], feed_dict)
 			if e < 1: break  # early stopping yay
 
-		print "early stopping => step: %d, error: %f\n" % (step, e)
+		print "early stopping => step: %d, cross_entropy: %f\n" % (step, e)
 		print self.sess.run(self.y, feed_dict={self.x: xTrain})
 		#print sess.run(self.y, feed_dict={self.x: [[2.0, 10.711832, 2.0]]})
--- textClassification/testClassify.py
+++ textClassification/testClassify.py
@@ -4,11 +4,10 @@ c = Classify()
 
 print "train\n"
 c.trainFilter('filtro.txt')
-"""
+
 c.loadFilter('filtro.txt.tfdb')
 
 print "filter\n"
 #c.loadFromWeb('www.emol.cl')
 c.loadFromFile('libro.txt')
 c.process()
-"""
